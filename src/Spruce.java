e1420765879cc867e732d31c53f50a37144235f6
Darya Korotkih <riyafonte@yahoo.com>
2017-11-11 23:29:27 +0300

    Практика. Ёлочка в консоль.

---
+++ src/Spruce.java
@@ -0,0 +1,17 @@
+public class Spruce {
+    public static void main(String[] args) {
+        //                      верш       1        2           3
+        System.out.println( "        /\\\n " +
+                             "      /  \\\n" +
+                            "      / || \\\n " +
+                             "    /  ||  \\\n " +
+                             "   /   ||   \\\n " +
+                             "  /    ||    \\"   );
+        System.out.println( "  /     ||     \\\n" +
+                            " /      ||      \\\n " +
+                             "       ||   " +
+                       "   \n------------------");
+
+    }
+}
+
